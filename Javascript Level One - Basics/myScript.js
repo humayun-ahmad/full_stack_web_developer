706a3ce41ad3054188b5dfdd80cce2d23f1976b6
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-10-16 19:35:24 +0600

    exercise javascript and html

--- Javascript Level One - Basics/myScript.js
+++ Javascript Level One - Basics/myScript.js
@@ -1,4 +1,4 @@
 alert("Welcome to your Bank");
 var deposit = prompt("how much would you like to deposit today?");
-alert("you've deposited: " + deposit)
+alert("you've deposited: " + deposit);
 console.log("you are a cool person!");
