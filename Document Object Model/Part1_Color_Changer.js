fd78d1c8217cd5942b7cf5bb6004484da64491a9
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-10-21 20:05:51 +0600

    document object model in javascript

---
+++ Document Object Model/Part1_Color_Changer.js
@@ -0,0 +1,14 @@
+//document.URL
+console.log(document.URL);
+// //
+console.log(document.body);
+// //
+console.log(document.head);
+// //
+console.log(document.links);
+// //
+ console.log(document.getElementsById("pickme"));
+//  //
+ console.log(document.getElementsByClassName("myurl"));
+//  //
+//  console.log(document.getElementByTagName("li"));
