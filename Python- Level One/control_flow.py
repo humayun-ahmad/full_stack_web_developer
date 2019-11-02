87b4761bd8fa396d07457658996377825d8904d9
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-11-02 13:07:02 +0600

    basic control flow in python

---
+++ Python- Level One/control_flow.py
@@ -0,0 +1,20 @@
+# dictionary control
+d = {"sam": 1,"frank":2,"dan":3}
+
+for item in d:
+	# print(item)
+	# print(d[item])
+	print("{} is : {}".format(item,d[item]))
+
+
+
+# pair control
+myPair = [(1,2),(3,4),(5,6)]
+
+for (a,b) in myPair:
+	print(a)
+	print(b)
+
+
+
+
