2ca2b2924e91470d171f700fedc28bbecb9c12ba
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-11-02 12:19:43 +0600

    string basic

---
+++ Python- Level One/string.py
@@ -0,0 +1,23 @@
+string = "Hello world"
+
+#for jumping
+# print(string[::2])
+
+#Scilcing the string
+mystring = 'Hello world'
+x = mystring.upper()
+x = mystring.lower()
+
+#capitalize method
+x = mystring.capitalize()
+
+# split method
+x = mystring.split('e')
+
+# print fomatting method
+z = "item one: {} item two: {}".format("cat","dog")
+z = "item one: {x} item two: {y}".format(x = "cat", y ="dog")
+print(z)
+
+
+
