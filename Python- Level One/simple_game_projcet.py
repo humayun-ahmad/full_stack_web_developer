5fff9a0abf7b1070c17ee8fa150a90a908b7b615
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-11-02 14:58:25 +0600

    add comment of enumerate() method details

--- Python- Level One/simple_game_projcet.py
+++ Python- Level One/simple_game_projcet.py
@@ -23,6 +23,9 @@ def generate_clues(code,user_guess):
 
 	clues = []
 	# ind = 0
+	'''
+	Enumerate() method adds a counter to an iterable and returns it in a form of enumerate object. This enumerate object can then be used directly in for loops or be converted into a list of tuples using list() method
+	'''
 	for ind, num in enumerate(user_guess):
 		if num == code[ind]:
 			clues.append("match")
