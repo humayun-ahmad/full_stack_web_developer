1daffa04a0eb61ebae06df4163cbc2fc96dc8990
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-11-02 12:39:39 +0600

    list basic

---
+++ Python- Level One/list.py
@@ -0,0 +1,24 @@
+mylist = ['a', 'b','c','d']
+
+numlist = [12,2,3,9,5]
+# append and extend two are not same so be carefull
+
+# append method 
+mylist.append()
+
+# extend method
+mylist.extend(['x','y','z'])
+
+# pop method
+item = mylist.pop(0)
+
+# reverse the list?
+mylist.reverse()
+
+# sort the list
+numlist.sort()
+
+# first column traversl
+matrix = [[1,23,3],[4,5,6],[7,8,9]]
+
+first_col = [row[0] for row in matrix]
