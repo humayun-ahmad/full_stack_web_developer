9c5fe3dedd0c1aa10972669ea45a9c4870e6972d
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-11-05 23:55:36 +0600

    setting and other file edit something

--- Django Level Two/Django_Project/first_project/first_project/settings.py
+++ Django Level Two/Django_Project/first_project/first_project/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build here templates-first_app-index file path
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Django Level Two/Django_Project/first_project/first_project/urls.py
+++ Django Level Two/Django_Project/first_project/first_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from first_app import views
 
 urlpatterns = [
+	path('', views.index, name="index"),
     path('admin/', admin.site.urls),
+    path('first_app/', include('first_app.urls')),
 ]
